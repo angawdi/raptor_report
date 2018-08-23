1f08f6f2453151b14d0a4a6ba3d22000bb28d5bc
Sky <anvandp18@gmail.com>
2018-08-23 14:05:56 -0700

    just prevent user from entering signing up in the wrong way'
    :

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
 // Require express
 var express = require('express');
 
+// includes the models
+var db = require('../models');
+
 // Declare a new router
 var router = express.Router();
 
@@ -10,6 +13,7 @@ router.get('/login', function(req, res){
 });
 
 router.post('/login', function(req, res){
+	console.log(req.body);
 	res.send('login post route');
 });
 
@@ -18,7 +22,23 @@ router.get('/signup', function(req, res){
 });
 
 router.post('/signup', function(req, res){
-	res.send('signup post route');
+	console.log(req.body);
+	req.body.admin = false;
+	db.user.findOrCreate({
+		where: { email: req.body.email },
+		defaults: req.body
+	}).spread(function(user, wasCreated){
+		if(wasCreated){ // this is expected behavior
+			// TODO automatically log the user in!
+			res.redirect('/profile');
+		}else{ // User messed up, they already have a login
+			// TODO: send the user some sort of error message
+			res.redirect('/auth/login');
+		}
+	}).catch(function(err){
+		console.log(err);
+		res.send(err);
+	})
 });
 
 router.get('/logout', function(req, res){
