b225c12c5176fffc46619ebfed7a1fe29b723d94
Sky <anvandp18@gmail.com>
2018-08-23 12:07:12 -0700

    materialize bootstrap...

---
+++ controllers/auth.js
@@ -0,0 +1,28 @@
+// Require express
+var express = require('express');
+
+// Declare a new router
+var router = express.Router();
+
+// Define routes
+router.get('/login', function(req, res){
+	res.render('auth/login');
+});
+
+router.post('/login', function(req, res){
+	res.send('login post route');
+});
+
+router.get('/signup', function(req, res){
+	res.render('auth/signup');
+});
+
+router.post('/signup', function(req, res){
+	res.send('signup post route');
+});
+
+router.get('/logout', function(req, res){
+	res.send('logout route'); /////////
+});
+
+module.exports = router;
