0a2de29e1c36be36e194e038404de1cd4cefaedb
Sky <anvandp18@gmail.com>
2018-09-04 05:05:53 -0700

    edit index page for fruits page

--- controllers/fruit.js
+++ controllers/fruit.js
@@ -10,4 +10,12 @@ router.get('/', function(req, res){
 	res.render('fruits/index');
 });
 
+router.get('/new', function(req, res){
+	res.render('fruits/new');
+});
+
+router.post('/', function(req, res){
+	res.send(req.body);
+});
+
 module.exports = router;
